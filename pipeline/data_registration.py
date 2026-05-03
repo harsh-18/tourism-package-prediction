17fcc53371974947bdbf8340ee60d5933f2dc9cb
harsh-18 <31316356+harsh-18@users.noreply.github.com>
2026-05-03 14:34:33 +0000

    Add project files from Drive

--- pipeline/data_registration.py
+++ pipeline/data_registration.py
@@ -9,7 +9,7 @@ dataset_repo = f"{HF_USERNAME}/tourism-dataset"
 
 api = HfApi()
 
-# Create the dataset repo on HF if it doesn't already exist
+# Creating the dataset repo on HF if it doesn't already exist
 api.create_repo(
     repo_id=dataset_repo,
     repo_type="dataset",
@@ -17,9 +17,9 @@ api.create_repo(
     token=HF_TOKEN
 )
 
-# Upload the raw CSV -- GitHub Actions will have this file via checkout
+# Uploading the raw CSV -- GitHub Actions will have this file via checkout
 api.upload_file(
-    path_or_fileobj="tourism_project/data/tourism.csv",
+    path_or_fileobj="data/tourism.csv",
     path_in_repo="tourism.csv",
     repo_id=dataset_repo,
     repo_type="dataset",
